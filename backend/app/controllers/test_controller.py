e344ae7f8019be48b8eb0a3d8366db8126b1f4c9
Eduarda Guimarães <eduardaguima2007@yahoo.com>
2025-04-22 16:48:36 -0300

    Primeiro commit

--- backend/app/controllers/test_controller.py
+++
@@ -1,9 +0,0 @@
-from flask import request, jsonify
-from app.services.ai_service import predict_career
-from app.models.test_model import TestModel
-
-def handle_test():
-    data = request.json
-    result = predict_career(data)
-    TestModel.save_result(data, result)
-    return jsonify(result)
--- backend/app/routes/test_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from app.controllers.test_controller import handle_test
-
-test_bp = Blueprint('test', __name__)
-
-@test_bp.route('/', methods=['POST'])
-def test():
-    return handle_test()
