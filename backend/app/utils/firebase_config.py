e7b54b4224985fec56b5e9cb957ab2778cdc3f45
Eduarda Guimarães <eduardaguima2007@yahoo.com>
2025-04-29 16:33:35 -0300

    Reorganização da estrutura

--- backend/app/utils/firebase_config.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin # type: ignore
-from firebase_admin import credentials, firestore # type: ignore
-
-cred = credentials.Certificate("path/to/seu-arquivo-firebase.json")  # Substitua aqui
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
