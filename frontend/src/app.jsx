3dbe3d22845e23cb90b25d3fe35847a076a8381a
Eduarda Guimarães <eduardaguima2007@yahoo.com>
2025-10-11 16:52:26 -0300

    fix: corrigir bug de abertura/fechamento do menu sanduíche no header responsivo

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -13,6 +13,8 @@ import Historico from './pages/Historico';
 import ProtectedRoute from './components/ProtectedRoute';
 import Header from './components/header';
 import Footer from './components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 
 function App() {
   return (
